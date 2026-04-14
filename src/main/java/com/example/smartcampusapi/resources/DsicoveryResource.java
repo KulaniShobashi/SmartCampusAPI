47b114c51d681cdc6de81d15f0aca0a69e2477e6
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-15 00:49:18 +0530

    create discovery resourse file

---
+++ src/main/java/com/example/smartcampusapi/resources/DsicoveryResource.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smartcampus.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/")
+public class DiscoveryResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDiscovery() {
+        Map<String, Object> meta = new HashMap<>();
+        meta.put("version", "1.0");
+        meta.put("adminContact", "hamed.hamzeh@westminster.ac.uk");
+        Map<String, String> resources = new HashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+        meta.put("resources", resources);
+        return Response.ok(meta).build();
+    }
+}
