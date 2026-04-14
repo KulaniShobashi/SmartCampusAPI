9d1bb484c0cbcc100c88503ed63f2ff4695b91ad
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-15 00:40:55 +0530

    create ApplicationConfig java file

---
+++ src/main/java/com/example/smartcampusapi/ApplicationConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smartcampus;
+
+import com.example.smartcampus.exception.mapper.*;
+import com.example.smartcampus.filter.LoggingFilter;
+import com.example.smartcampus.resource.*;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/api/v1")
+public class ApplicationConfig extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(DiscoveryResource.class);
+        classes.add(RoomResource.class);
+        classes.add(SensorResource.class);
+        classes.add(RoomNotEmptyExceptionMapper.class);
+        classes.add(LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(SensorUnavailableExceptionMapper.class);
+        classes.add(GenericExceptionMapper.class);
+        classes.add(LoggingFilter.class);
+        return classes;
+    }
+}
