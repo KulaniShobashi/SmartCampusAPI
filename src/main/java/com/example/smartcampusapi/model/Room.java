5da8b3099090831a1c89e1d8ac382683e51ba0a1
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-15 00:44:51 +0530

    create sensor java file

--- src/main/java/com/example/smartcampusapi/model/Room.java
+++ src/main/java/com/example/smartcampusapi/model/Room.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package com.example.smartcampus.model;
+package com.example.smartcampusapi.model;
 
 import java.util.ArrayList;
 import java.util.List;
