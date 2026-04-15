a556449f9e7e3fe1ef468447a8587a17d7bc917c
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-15 15:45:02 +0530

    create LoggingFilter file

---
+++ src/main/java/com/example/smartcampusapi/filter/LoggingFilter.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smartcampus.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        LOGGER.info("REQUEST: " + requestContext.getMethod() + " " + requestContext.getUriInfo().getAbsolutePath());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        LOGGER.info("RESPONSE: " + responseContext.getStatus());
+    }
+}
