731aa664b56b3d20023d8de4666b144863b6b0cd
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-15 15:43:24 +0530

    create  GenericExceptionMapper file

---
+++ src/main/java/com/example/smartcampusapi/exception/mapper/GenericExceptionMapper.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smartcampus.exception.mapper;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "InternalServerError");
+        error.put("message", "An unexpected error occurred. Please try again later.");
+        return Response.status(500).entity(error).type(MediaType.APPLICATION_JSON).build();
+    }
+}
