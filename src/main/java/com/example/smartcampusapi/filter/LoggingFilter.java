28983ecb80ecc7f3e52b1ad4c0dff1ad7878b8e5
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-17 16:23:04 +0530

    update code

--- src/main/java/com/example/smartcampusapi/filter/LoggingFilter.java
+++ src/main/java/com/example/smartcampusapi/filter/LoggingFilter.java
@@ -6,20 +6,32 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Provider
 public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
-    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        LOGGER.info("REQUEST  → " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+        String method = requestContext.getMethod();
+        String uri = requestContext.getUriInfo().getRequestUri().toString();
+        String timestamp = LocalDateTime.now().format(formatter);
+
+        // This prints in RED in the NetBeans console for the examiner to see clearly
+        System.err.println(">>> [AUDIT START] " + timestamp);
+        System.err.println(">>> METHOD: " + method);
+        System.err.println(">>> PATH: " + uri);
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        LOGGER.info("RESPONSE ← " + responseContext.getStatus() + " " + requestContext.getMethod() + " " + requestContext.getUriInfo().getRequestUri());
+        int status = responseContext.getStatus();
+        
+        // Logs the response status code
+        System.err.println("<<< [AUDIT END] Status Code: " + status);
+        System.err.println("----------------------------------------------");
     }
 }
